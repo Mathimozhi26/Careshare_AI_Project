c84dd6e6a045261faa15c28adeee96dea4fbd3e7
Yokesh <yoki@Yokeshs-Mac-mini.local>
2026-06-14 21:54:58 +0530

    Fix base href for GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3664694355" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1842156810" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
